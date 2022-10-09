9aa570ff1a0e930c332b22e2b11e93b5d511d9ce
james.gu <james.gu@bimobject.com>
2022-10-09 13:25:36 +0200

    #feat: delivery

--- client/App.js
+++ client/App.js
@@ -4,6 +4,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import RestaurantScreen from './src/screens/RestaurantScreen';
 import BasketScreen from './src/screens/BasketScreen';
+import PreparingOrderScreen from './src/screens/PreparingOrderScreen';
+import DeliveryScreen from './src/screens/DeliveryScreen';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
@@ -21,6 +23,16 @@ export default function App() {
               name="Basket"
               component={BasketScreen}
               options={{ presentation: 'modal', headerShown: false }}
+            />
+            <Stack.Screen
+              name="PreparingOrder"
+              component={PreparingOrderScreen}
+              options={{ presentation: 'fullScreenModal', headerShown: false }}
+            />
+            <Stack.Screen
+              name="Delivery"
+              component={DeliveryScreen}
+              options={{ presentation: 'fullScreenModal', headerShown: false }}
             />
           </Stack.Navigator>
         </TailwindProvider>
